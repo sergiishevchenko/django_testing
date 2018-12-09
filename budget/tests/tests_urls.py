386fa656ea42ba76879d1b6a95d000979ab59364
sergii <sergiishevchenko017@gmail.com>
2018-12-09 11:49:44 +0300

    #

--- budget/tests/tests_urls.py
+++ budget/tests/tests_urls.py
@@ -5,7 +5,14 @@ from budget.views import project_list, project_detail, ProjectCreateView
 
 class TestUrls(SimpleTestCase):
 
-    def test_list_url_is_resolved(self):
+    def test_list_url_is_resolves(self):
         url = reverse('list')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, project_list)
+
+    def test_add_url_resolves(self):
+        url = reverse('add')
+        self.assertEquals(resolve(url).func.view_class, ProjectCreateView)
+
+    def test_detail_url_resolves(self):
+        url = reverse('detail')
+        self.assertEquals(resolve(url).func, project_detail)
