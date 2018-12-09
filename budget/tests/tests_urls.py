b7b54a414d865c757fd5597c19cc40edc3827e12
sergii <sergiishevchenko017@gmail.com>
2018-12-09 11:36:44 +0300

    #

--- budget/tests/tests_urls.py
+++ budget/tests/tests_urls.py
@@ -1,7 +1,10 @@
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from budget.views import project_list, project_detail, ProjectCreateView
 
 
 class TestUrls(SimpleTestCase):
 
     def test_list_url_is_resolved(self):
-        assert 1 == 2
+        url = reverse('list')
+        print(resolve(url))
